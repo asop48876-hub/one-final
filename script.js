f8f5a46922e47b8d273c2224848e5f950651c9c2
asop48876-hub <asop48876@gmail.com>
2026-08-08 09:22:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,88 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+    const slider = document.getElementById("slider");
+    const pages = document.querySelectorAll(".page");
+    const nextButtons = document.querySelectorAll(".next-btn");
+
+    const yesButton = document.getElementById("yesButton");
+    const thinkButton = document.getElementById("thinkButton");
+    const response = document.getElementById("response");
+
+    let currentPage = 0;
+
+    if (!slider || pages.length === 0) {
+        return;
+    }
+
+    function showPage(index) {
+
+        if (index < 0 || index >= pages.length) {
+            return;
+        }
+
+        currentPage = index;
+
+        slider.style.transform =
+            "translateX(-" + (index * 100) + "%)";
+    }
+
+    nextButtons.forEach(function (button) {
+
+        button.addEventListener("click", function () {
+
+            if (currentPage < pages.length - 1) {
+                showPage(currentPage + 1);
+            }
+
+        });
+
+    });
+
+
+    if (yesButton) {
+
+        yesButton.addEventListener("click", function () {
+
+            if (response) {
+                response.textContent =
+                    "You just made me smile. ♡";
+
+                response.classList.add("show");
+            }
+
+            yesButton.disabled = true;
+
+            if (thinkButton) {
+                thinkButton.disabled = true;
+            }
+
+        });
+
+    }
+
+
+    if (thinkButton) {
+
+        thinkButton.addEventListener("click", function () {
+
+            if (response) {
+                response.textContent =
+                    "Take all the time you need. ♡";
+
+                response.classList.add("show");
+            }
+
+            thinkButton.disabled = true;
+
+            if (yesButton) {
+                yesButton.disabled = true;
+            }
+
+        });
+
+    }
+
+
+    showPage(0);
+
+});
